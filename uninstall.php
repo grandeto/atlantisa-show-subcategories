7cdb9cced7796631f471e50895f4e3fca871833a
grandeto <grandeto.com@gmail.com>
2018-10-30 10:08:48 +0200

    Sync indent_style

--- uninstall.php
+++ uninstall.php
@@ -7,7 +7,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
 }
 if ( !is_user_logged_in() )
-	wp_die( 'You must be logged in to run this script.' );
+    wp_die( 'You must be logged in to run this script.' );
 if ( !current_user_can( 'install_plugins' ) )
-	wp_die( 'You do not have permission to run this script.' );
+    wp_die( 'You do not have permission to run this script.' );
 // Enter our plugin uninstall script below
